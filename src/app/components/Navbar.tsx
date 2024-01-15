66ec98b4d003f7b9f51f56db510d4ef02c89b764
marcus <97414481+kiryano@users.noreply.github.com>
2024-01-14 19:32:10 -0800

    landing page
    landing page

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export const Navbar = () => {
+  return (
+    <section className="">
+      <SimpleFloatingNav />
+    </section>
+  );
+};
+
+const SimpleFloatingNav = () => {
+  return (
+    <nav className="fixed left-[50%] top-8 flex w-fit -translate-x-[50%] items-center gap-6 rounded-lg border-[1px] border-indigo-700 bg-indigo-900 p-3 text-sm text-neutral-50">
+      <Logo />
+
+      <NavLink>Home</NavLink>
+      <NavLink>About</NavLink>
+      <NavLink>Terms</NavLink>
+      <NavLink>Privacy</NavLink>
+
+    </nav>
+  );
+};
+
+const Logo = () => {
+  return (
+<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M8 4V8H4V10H8V14H4V16H8V20H10V16H14V20H16V16H20V14H16V10H20V8H16V4H14V8H10V4H8ZM14 14V10H10V14H14Z"
+    fill="currentColor"
+  />
+</svg>
+  );
+};
+
+const NavLink = ({ children }: { children: string }) => {
+  return (
+    <a href="#" rel="nofollow" className="block overflow-hidden">
+      <motion.div
+        whileHover={{ y: -20 }}
+        transition={{ ease: "backInOut", duration: 0.5 }}
+        className="h-[20px]"
+      >
+        <span className="flex h-[20px] items-center">{children}</span>
+        <span className="flex h-[20px] items-center text-neutral-50">
+          {children}
+        </span>
+      </motion.div>
+    </a>
+  );
+};
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,121 @@
-import Image from 'next/image'
+'use client';
 
+
+import React, { useState, useEffect, SetStateAction } from 'react';
+import { Navbar } from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const BubbleText = () => {
+    useEffect(() => {
+        const spans = document.querySelectorAll(".hover-text span") as NodeListOf<HTMLSpanElement>;
+
+        spans.forEach((span) => {
+            span.addEventListener("mouseenter", function (this: typeof span) {
+                this.style.fontWeight = "900";
+                this.style.color = "rgb(238, 242, 255)";
+
+                const leftNeighbor = this.previousElementSibling as HTMLSpanElement;
+                const rightNeighbor = this.nextElementSibling as HTMLSpanElement;
+
+                if (leftNeighbor) {
+                    leftNeighbor.style.fontWeight = "500";
+                    leftNeighbor.style.color = "rgb(199, 210, 254)";
+                }
+                if (rightNeighbor) {
+                    rightNeighbor.style.fontWeight = "500";
+                    rightNeighbor.style.color = "rgb(199, 210, 254)";
+                }
+            });
+
+            span.addEventListener("mouseleave", function (this: typeof span) {
+                this.style.fontWeight = "100";
+                this.style.color = "rgb(165, 180, 252)";
+
+                const leftNeighbor = this.previousElementSibling as HTMLSpanElement;
+                const rightNeighbor = this.nextElementSibling as HTMLSpanElement;
+
+                if (leftNeighbor) {
+                    leftNeighbor.style.fontWeight = "100";
+                    leftNeighbor.style.color = "rgb(165, 180, 252)";
+                }
+
+                if (rightNeighbor) {
+                    rightNeighbor.style.fontWeight = "100";
+                    rightNeighbor.style.color = "rgb(165, 180, 252)";
+                }
+            });
+        });
+    }, []);
+
+    return (
+        <h2 className="hover-text text-center text-5xl font-thin text-[#331E84]">
+            <Text>nitrous</Text>
+        </h2>
+    );
+};
+
+const Text = ({ children }: { children: string }) => {
+    return (
+        <>
+            {children.split("").map((child, idx) => (
+                <span
+                    style={{
+                        transition: "0.35s font-weight, 0.35s color",
+                    }}
+                    key={idx}
+                >
+                    {child}
+                </span>
+            ))}
+        </>
+    );
+};
+
+// Home Component
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [selectedButton, setSelectedButton] = useState('Email');
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const buttons = [
+        'email',
+        'username',
+        'phone',
+        'ip address',
+    ];
+
+    const handleButtonChange = (name: SetStateAction<string>) => {
+      if (!loading)
+        setSelectedButton(name);
+    }
+
+
+    return (
+        <div>
+            <Navbar />
+            {/* background and layout */}
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,black_1px)] bg-[size:20px_20px]"></div>
+            <div className="mx-auto w-screen h-screen px-6 pb-10 md:max-w-[720px] lg:max-w-[612px] flex flex-col justify-center gap-4">
+                
+                <BubbleText />
+
+                <div className='text-center text-xl text-gray-100 font-normal'>
+                    open source osint tool
+                </div>
+
+                {/* error message and search input */}
+                <p className="flex justify-center text-white">{errorMessage}</p>
+                <div className="relative flex items-center">
+                    <input className="w-full bg-[#331E84] px-4 py-3 text-left text-lg font-normal leading-none placeholder-gray-200 outline-none rounded-none" type="text" placeholder="enter an email, username, phone, ip & more..."></input>
+                    <button className="cursor-pointer bg-transparent text-white h-full w-24">search</button>
+                </div>
+                <div className="flex flex-wrap gap-2 justify-center">
+                    {buttons.map((button, index) => (
+                        <button key={index} onClick={() => handleButtonChange(button)} className={`p-2 rounded w-32 text-white cursor-pointer bg-[#5D3FD3] ${selectedButton === button ? 'bg-[#331E84]' : ''}`}>{button}</button>
+                    ))}
+                </div>
+            </div>
+            <Footer />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
